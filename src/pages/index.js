bbbf21c80fbd7c341ef50eda605a749cbd6465b1
jiangyangcreate <jiangyangcreate@gmail.com>
2023-12-25 10:18:58 +0800

    update:国际化更新:完整的Python之禅

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,16 +15,16 @@ function MyHero() {
         <p className={styles.leftContainer_p}>
         
         <Translate id="homepage.philosophy.one">优美优于丑陋，明了优于隐晦</Translate><br />
-        <Translate>简单优于复杂，复杂优于繁杂</Translate><br />
-        <Translate>扁平优于嵌套，稀疏优于稠密</Translate><br />
-        <Translate>可读性很重要！</Translate><br />
-        <Translate>特例亦不可违背原则，即使实用比纯粹更优。</Translate><br />
-        <Translate>错误绝不能悄悄忽略，除非它明确需要如此。</Translate><br />
-        <Translate>面对不确定性，拒绝妄加猜测。</Translate><br />
-        <Translate>任何问题应有一种，且最好只有一种，显而易见的解决方法。</Translate><br />
-        <Translate>做优于不做，然而不假思索还不如不做。</Translate><br />
-        <Translate>很难解释的，必然是坏方法。</Translate><br />
-        <Translate>很好解释的，可能是好方法。</Translate><br />
+        <Translate id="homepage.philosophy.two">简单优于复杂，复杂优于繁杂</Translate><br />
+        <Translate id="homepage.philosophy.three">扁平优于嵌套，稀疏优于稠密</Translate><br />
+        <Translate id="homepage.philosophy.four">可读性很重要！</Translate><br />
+        <Translate id="homepage.philosophy.five">特例亦不可违背原则，即使实用比纯粹更优。</Translate><br />
+        <Translate id="homepage.philosophy.six">错误绝不能悄悄忽略，除非它明确需要如此。</Translate><br />
+        <Translate id="homepage.philosophy.seven">面对不确定性，拒绝妄加猜测。</Translate><br />
+        <Translate id="homepage.philosophy.eight">任何问题应有一种，且最好只有一种，显而易见的解决方法。</Translate><br />
+        <Translate id="homepage.philosophy.nine">做优于不做，然而不假思索还不如不做。</Translate><br />
+        <Translate id="homepage.philosophy.ten">很难解释的，必然是坏方法。</Translate><br />
+        <Translate id="homepage.philosophy.eleven">很好解释的，可能是好方法。</Translate><br />
           
         </p>
         <div className={styles.buttonContainer}>
