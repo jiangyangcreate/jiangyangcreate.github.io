c4835e16f87458488f76edfc884654f763a77ea8
jiangyangcreate <jiangyangcreate@gmail.com>
2024-01-04 23:18:34 +0800

    简化案例页

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -15,124 +15,52 @@ function ShowcaseGrid(props) {
 	} = props;
 
 	const headingTitle = privateType ? 'jiangyang showcase' : 'Showcase';
-	const headingSubtitle = privateType ? "See all the awesome websites built by Infinum's WordPress team." : 'See the awesome websites people are building with Eightshift Boilerplate.';
+	const headingSubtitle = privateType ? "See all the awesome websites built by Infinum's WordPress team." : 'See the awesome websites people are building';
 	const ctaTitle = privateType ? "Let's get in touch" : (<span>你好 <br /> 遇到问题了吗?</span>);
 	const ctaSubtitle = privateType ? 'Contact us' : 'Open an issue on GitHub';
 	const ctaUrl = privateType ? 'https://github.com/jiangyangcreate/' : 'https://github.com/jiangyangcreate/jiangyangcreate.github.io/issues';
 
 	const privateData = [
-		{
-			image: useBaseUrl('img/showcase/tarisio.webp'),
-			label: 'Tariso',
-			desc: 'Tarisio is a web-based auction house specializing in stringed instruments and bows. Its online auctions provide a global marketplace for fine violin sales.',
-			link: 'https://tarisio.com/',
-		},
-		{
-			image: useBaseUrl('img/showcase/t2.webp'),
-			label: 'T2 auctions',
-			desc: 'Auctions for the Violin Trade.',
-			link: 'https://t2-auctions.com/',
-		},
-		{
-			image: useBaseUrl('img/showcase/rasco.webp'),
-			label: 'Rasco',
-			desc: 'One of the leading manufacturers of municipal equipment in Europe: we produce snow ploughs (snowplow), spreaders, crane mowers and vehicle upgrades.',
-			link: 'https://rasco.hr/',
-		},
-		{
-			image: useBaseUrl('img/showcase/lynx.webp'),
-			label: 'Lynx',
-			desc: 'LYNX is a 2m³ vacuum road sweeper exceeding expectations. Modern design, functionality, power and ease of use for clean streets.',
-			link: 'https://lynx.rasco.hr/',
-		},
-		{
-			image: useBaseUrl('img/showcase/hak.webp'),
-			label: 'HAK Revija',
-			desc: 'HAK news portal.',
-			link: 'https://revijahak.hr/',
-		},
-		{
-			image: useBaseUrl('img/showcase/crveninosovi.webp'),
-			label: 'Crveni nosovi',
-			desc: 'Our mission is to restore the feeling of joy, happiness and optimism in difficult life circumstances.',
-			link: 'https://www.crveninosovi.hr/',
-		},
-		{
-			image: useBaseUrl('img/showcase/vcfa.webp'),
-			label: 'VCFA',
-			desc: 'Learn with accomplished teachers and artistic colleagues from around the world. You’ll create new work, refine your vision, and enhance your critical and creative skills.',
-			link: 'https://vcfa.edu/',
-		},
-		{
-			image: useBaseUrl('img/showcase/go.webp'),
-			label: 'Malta Go',
-			desc: 'GO is Malta’s leading communications services company. As the first quad play provider in Malta we provide mobile, fixed line, internet and TV services to more than 500,000 customers.',
-			link: 'https://www.go.com.mt/',
-		},
+
 	];
 
 	const publicData = [
 		{
+			image: useBaseUrl('img/showcase/jiangmiemie.png'),
+			label: 'jiangmiemie',
+			desc: '这是一个基于 Docusaurus 3.0 最新版本的自定义站点。设计理念：美丽胜过丑陋，清晰胜过模糊。简洁胜过复杂，复杂胜过繁琐。希望你喜欢这个小巧而吸引人的个人博客站点。',
+			link: 'https://jiangmiemie.com/',
+		},
+		{
 			image: useBaseUrl('img/showcase/infinum.webp'),
-			label: 'Infinum Web',
+			label: '填充站',
 			desc: 'An independent design and development agency with offices in the US and Europe. We create beautiful apps people love to use.',
 			link: 'https://infinum.com/',
 		},
 		{
 			image: useBaseUrl('img/showcase/solplanet.webp'),
-			label: 'Solplanet',
+			label: '填充站',
 			desc: 'The power of the sun is the future of our planet. Solplanet makes solar for everybody. Easy-to-install, reliable and user-friendly inverters.',
 			link: 'https://solplanet.net/',
 		},
 		{
 			image: useBaseUrl('img/showcase/academy.webp'),
-			label: 'Infinum Academy',
+			label: '填充站',
 			desc: 'Learn how to build apps. For free. No practical experience? No problem. People who build apps every day are teaching how to create software from scratch. By the end of the course, you will have built your very own app.',
 			link: 'https://academy.infinum.com/',
 		},
 		{
 			image: useBaseUrl('img/showcase/raise-the-bar.webp'),
-			label: 'Raise The Bar',
+			label: '填充站',
 			desc: 'Coca-Cola HBC is helps to align the skills and knowledge in tourism and hospitality through Raise the Bar education program.',
 			link: 'https://raisethebar.hr/',
 		},
 		{
-			image: useBaseUrl('img/showcase/d66.webp'),
-			label: 'D66',
-			desc: 'Democrats 66 is a social-liberal political party in the Netherlands. Its name originates from the year in which it was founded.',
-			link: 'https://d66.nl/',
-		},
-		{
 			image: useBaseUrl('img/showcase/a1-jobs.webp'),
-			label: 'A1 Jobs',
+			label: '填充站',
 			desc: 'At A1 Group we are driving digitalization in order to make life easier, more enjoyable and more productive. Are you in? Come and join us!',
 			link: 'https://jobs.a1.com/',
 		},
-		{
-			image: useBaseUrl('img/showcase/a1-brand-portal.webp'),
-			label: 'A1 Brand Portal',
-			desc: 'Welcome to A1. Understand, explore and create the world of A1. Play Video. Brand Strategy.',
-			link: 'https://brand.a1.group/',
-		},
-		{
-			image: useBaseUrl('img/showcase/a1-digitalcampus.webp'),
-			label: 'A1 Digital Campus',
-			desc: 'At the A1 digital.campus you will find free, innovative workshops on future digital topics such as "Coding" and "Robotics" for children and young people. In addition, we organize hackathons and even our own start-up school.',
-			link: 'https://a1digitalcampus.at/',
-		},
-		{
-			image: useBaseUrl('img/showcase/a1-wholesale.webp'),
-			label: 'A1 Wholesale',
-			desc: 'Success begins with the right partner. A1 Wholesale is known for its reliability and technical know-how. No wonder more than 24 million retail customers in CEE count on us: mobile phone providers, fixed network operators, internet providers, MVNO and broadcasting companies.',
-			link: 'https://wholesale.a1.at/',
-		},
-		{
-			image: useBaseUrl('img/showcase/a1-internationalbusiness.webp'),
-			label: 'A1 International business',
-			desc: 'Success begins with the right partner. A1 International Business is known for its reliability and technical know-how. No wonder more than 24 million retail customers in CEE count on us: mobile phone providers, fixed network operators, internet providers, MVNO and broadcasting companies.',
-			link: 'https://internationalbusiness.a1.group/',
-		},
-
 	];
 
 	const itemsData = privateType ? [...publicData, ...privateData] : publicData;
