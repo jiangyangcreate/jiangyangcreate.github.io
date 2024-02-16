199e289617dfc5ad6ff5a63b1e8c5f3ef355dbc4
jiangyangcreate <jiangyangcreate@gmail.com>
2024-02-16 20:22:04 +0800

    💄 美化样式

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import Layout from "@theme/Layout";
-import React from 'react';
-import Box from '@mui/material/Box';
+import React from "react";
+import Box from "@mui/material/Box";
 
 function MyComponent() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '79vh',}}>
-      <h1>Hello , World</h1>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "79vh",
+      }}
+    >
+      <h1
+        style={{
+          textShadow: "-1px 1px #3C3C3C, -10px 10px 5px #3C3C3C80",
+          fontSize: "12vmin",
+        }}
+      >
+        Hello,World
+      </h1>
     </Box>
   );
 }
@@ -22,7 +37,7 @@ export default function Title() {
       keywords={siteConfig.customFields.keywords}
       wrapperClassName="es-navbar-white"
     >
-     <MyComponent/>
+      <MyComponent />
     </Layout>
   );
 }
