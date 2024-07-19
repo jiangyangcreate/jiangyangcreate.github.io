b8f93b64a08b5f8bfae0632bf090f7c4fa3f09e1
jiangyangcreate <jiangyangcreate@gmail.com>
2024-07-19 14:04:21 +0800

    ✨ new first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,63 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import React from "react";
+import { useHistory } from "@docusaurus/router";
+import React, { useState, useEffect } from 'react';
+
+const CustomSearch = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    const searchInput = document.querySelector(".search-box input");
+    if (searchInput) {
+      searchInput.value = searchValue;
+    }
+  }, [searchValue]);
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+    history.push(`/search?q=${event.target.value}`);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleSearchChange}
+        placeholder="Search : Hello, World!"
+        style={{
+          width: "100%",
+          padding: "10px",
+          borderRadius: "20px",
+          border: "1px solid #ccc",
+        }}
+      />
+    </div>
+  );
+};
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  const tick = () => {
+    setDate(new Date());
+  };
+
+  return (
+    <div style={{ color: 'black', height: "200px", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>{date.toLocaleTimeString()}</h1>
+    </div>
+  );
+};
 
 function MyComponent() {
   return (
@@ -11,15 +68,20 @@ function MyComponent() {
         alignItems: "center",
         justifyContent: "center",
         height: "79vh",
+        margin: "0 5%",
       }}
     >
+      <div style={{ marginBottom: "-60px" }}> 
+        <Clock />
+      </div>
       <h1
         style={{
           textShadow: "-1px 1px #3C3C3C, -10px 10px 5px #3C3C3C80",
-          fontSize: "12vmin",
+          // fontSize: "12vmin",
         }}
       >
-        Hello, World!
+        {/* Hello, World! */}
+        <CustomSearch />
       </h1>
     </div>
   );
@@ -39,3 +101,5 @@ export default function Title() {
     </Layout>
   );
 }
+
+
