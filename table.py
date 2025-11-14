1fbc2e9b17979a73b3b40d7349e0a0af6b2817df
jiangyang <jiangyangcreate@gmail.com>
2025-11-14 13:39:53 +0800

    🗑️ 删除示例代码文件`table.py`
    - 移除了`table.py`文件，该文件包含了查找目标值的示例代码。
    - 更新了文档，增加了关于装饰器的详细说明和示例，提升了内容的实用性与可读性。

--- table.py
+++
@@ -1,9 +0,0 @@
-lists = [1,2,3,4,5,6,7,8,9,10]
-target = 9
-
-for index , number in enumerate(lists):
-    if number == target:
-        print(f"找到目标值{target}，索引为{index}")
-        break
-else:
-    print(f"未找到目标值{target}")
