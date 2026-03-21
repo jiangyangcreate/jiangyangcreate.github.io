3f0f3400e183cba4c419048d2f35ad8b1877695d
jiangyangcreate <jiangyangcreate@gmail.com>
2026-03-21 22:34:45 +0800

    更新 README 文档，移除不再使用的 npm 命令。修改 case.js 中的项目描述，合并和更新项目信息，删除不再维护的项目图片文件。

--- src/pages/case.js
+++ src/pages/case.js
@@ -16,30 +16,23 @@ function ShowcaseGrid() {
     {
       image: useBaseUrl("pages/case/jiangmiemie.webp"),
       title: "jiangmiemie",
-      desc: "精巧且开源的个人博客站点。配置生成式 AI 文章总结，个人相册，支持 mermaid 、KaTeX 、交互式思维导图。可使用 GitHub 账号评论，一键分享至 twitter。",
+      desc: "精巧且开源的个人博客站点。配置生成式 AI 文章总结、问答、播客。内置相册，支持 mermaid 、KaTeX 、交互式思维导图。可使用 GitHub 账号评论。",
       url: "https://github.com/jiangyangcreate/jiangyangcreate.github.io",
-      category: "关于我", 
-    },
-    {
-      image: useBaseUrl("pages/case/readme.webp"),
-      title: "Readme",
-      desc: "关于我自己的中英介绍，使用jinja模板。通过hash去重存储文章、大语言模型生成总结、每天定时更新。",
-      url: "https://jiangmiemie.com/jiangyangcreate/",
-      category: "关于我", 
+      category: "维护中", 
     },
     {
       image: useBaseUrl("pages/case/AI-Practice-Collection.webp"),
-      title: "AI-Practice-Collection",
-      desc: "这是一个用于AI技术练习和探索的项目合集。该仓库汇集了多个完整的AI项目，适用于学习和练习。",
+      title: "Collection",
+      desc: "这是一个用于技术练习和探索的项目合集。该仓库汇集了多个完整的项目，适用于学习和练习。",
       url: "https://github.com/jiangyangcreate/AI-Practice-Collection",
-      category: "关于我", 
+      category: "维护中", 
     },
     {
-      image: useBaseUrl("pages/case/Bluetooth.webp"),
-      title: "Bluetooth 小程序",
-      desc: "微信小程序实现蓝牙遥控最小示例：微信小程序与设备通信（测试设备为ESP32单片机）。支持中文、英文、文件分片传输。",
-      url: "https://github.com/jiangyangcreate/WX-Mini-Program/",
-      category: "存档项目", 
+    image: useBaseUrl("pages/case/etool.webp"),
+    title: "eTool",
+    desc: "30 多种常用工具集合库，包含：进程定时监听；发送邮件；硬件测速（网络、硬盘、内存、显存）；PDF、excel、word、图片等常用处理；windows右键菜单管理等",
+    url: "https://github.com/jiangyangcreate/eTool/",
+    category: "维护中", 
     },
       {
       image: useBaseUrl("pages/case/halo-theme-2020.webp"),
@@ -54,14 +47,7 @@ function ShowcaseGrid() {
       desc: "Python驱动库，适用于Jetson Orin Nano 和 RK3399 Pro芯片，支持RSS协议（摄像头云台）、数字量模拟量的处理（常见传感器）、i2c协议（控制RGB灯带）、超声波读取、非加密NFC读写。",
       url: "https://github.com/jiangyangcreate/exboard/",
       category: "存档项目", 
-    },
-      {
-      image: useBaseUrl("pages/case/etool.webp"),
-      title: "eTool",
-      desc: "30 多种常用工具集合库，包含：进程定时监听；发送邮件；硬件测速（网络、硬盘、内存、显存）；PDF、excel、word、图片等常用处理；windows右键菜单管理等",
-      url: "https://github.com/jiangyangcreate/eTool/",
-      category: "模块库", 
-    },
+    }
   ];
 
   // https://reactjs.org/docs/react-dom.html#hydrate
